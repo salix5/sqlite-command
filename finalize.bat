f7076807891a15b5f4fc676429aa74f77e6583e6
salix5 <salix5@livemail.tw>
2021-04-09 01:04:58 +0800

    minor fix

--- finalize.bat
+++ finalize.bat
@@ -1,3 +1,4 @@
 sqlite3.exe ..\Card\cards.cdb <cmd\finalize.txt
 sqlite3.exe ..\Card\expansions\beta.cdb <cmd\finalize2.txt
 sqlite3.exe ..\io\query\beta.cdb <cmd\finalize2.txt
+copy ..\ygopro\lflist.conf ..\card\ /y
