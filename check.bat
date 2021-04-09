f9301436c2da61d4ebec01e16658d34ddb414977
salix5 <salix5@livemail.tw>
2021-04-09 17:14:42 +0800

    add check_space.txt

--- check.bat
+++ check.bat
@@ -3,4 +3,6 @@ sqlite3.exe ..\Card\cards.cdb <cmd\unique_part2.txt >result\u2.txt
 fc result\u1.txt result\u2.txt
 
 sqlite3.exe ..\Card\cards.cdb <cmd\check_tmp.txt
+sqlite3.exe ..\Card\cards.cdb <cmd\check_space.txt
+sqlite3.exe ..\io\query\beta.cdb <cmd\check_space.txt
 pause
