1b665948bfa8b76b9086daeab40f2f433514a99e
salix5 <salix5@livemail.tw>
2021-06-06 02:33:21 +0800

    add copy_category.txt

--- check.bat
+++ check.bat
@@ -2,10 +2,14 @@ sqlite3.exe ..\Card\cards.cdb <cmd\unique_part1.txt >result\u1.txt
 sqlite3.exe ..\Card\cards.cdb <cmd\unique_part2.txt >result\u2.txt
 fc result\u1.txt result\u2.txt
 
-sqlite3.exe ..\Card\cards.cdb <cmd\check_tmp.txt
+sqlite3.exe cards.cdb <cmd\compare.txt >result\datas1.txt
+sqlite3.exe ..\Card\cards.cdb <cmd\compare.txt >result\datas2.txt
+fc result\datas1.txt result\datas2.txt
+
 sqlite3.exe ..\Card\cards.cdb <cmd\check_space.txt
 sqlite3.exe ..\io\query\beta.cdb <cmd\check_space.txt
 
+sqlite3.exe ..\Card\cards.cdb <cmd\check_tmp.txt
 sqlite3.exe pre-release.cdb <cmd\compare_full.txt >result\pre1.txt
 sqlite3.exe ..\Card\expansions\beta.cdb <cmd\compare_full.txt >result\pre2.txt
 fc result\pre1.txt result\pre2.txt
---
+++ copy_category.bat
@@ -0,0 +1,1 @@
+sqlite3.exe ..\Card\cards.cdb <cmd\copy_category.txt
