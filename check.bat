45afe080b7324883e59c6847725ef1934a810c14
salix5 <salix5@livemail.tw>
2021-07-09 23:14:14 +0800

    update path

--- check.bat
+++ check.bat
@@ -7,7 +7,7 @@ sqlite3.exe ..\Card\cards.cdb <cmd\compare.txt >result\datas2.txt
 fc result\datas1.txt result\datas2.txt
 
 sqlite3.exe ..\Card\cards.cdb <cmd\check_space.txt
-sqlite3.exe ..\io\query\beta.cdb <cmd\check_space.txt
+sqlite3.exe ..\io\pre\pre-release.cdb <cmd\check_space.txt
 
 sqlite3.exe ..\Card\cards.cdb <cmd\check_tmp.txt
 sqlite3.exe pre-release.cdb <cmd\compare_full.txt >result\pre1.txt
