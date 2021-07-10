1a3562605aa1dde3ddaf66c19fa95504719c15d1
salix5 <salix5@livemail.tw>
2021-07-11 05:10:12 +0800

    update check space

--- check.bat
+++ check.bat
@@ -6,11 +6,12 @@ sqlite3.exe cards.cdb <cmd\compare.txt >result\datas1.txt
 sqlite3.exe ..\Card\cards.cdb <cmd\compare.txt >result\datas2.txt
 fc result\datas1.txt result\datas2.txt
 
-sqlite3.exe ..\Card\cards.cdb <cmd\check_space.txt
-sqlite3.exe ..\io\pre\pre-release.cdb <cmd\check_space.txt
-
-sqlite3.exe ..\Card\cards.cdb <cmd\check_tmp.txt
 sqlite3.exe pre-release.cdb <cmd\compare_full.txt >result\pre1.txt
 sqlite3.exe ..\Card\expansions\beta.cdb <cmd\compare_full.txt >result\pre2.txt
 fc result\pre1.txt result\pre2.txt
+
+sqlite3.exe ..\Card\cards.cdb <cmd\check_tmp.txt
+sqlite3.exe ..\Card\cards.cdb <cmd\check_space.txt
+sqlite3.exe ..\io\pre\pre-release.cdb <cmd\check_space.txt
+
 pause
