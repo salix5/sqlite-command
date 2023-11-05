18c6f0948a0be02d1dfab1fdb39b528f41ff0767
salix5 <salix5566@gmail.com>
2023-11-06 00:05:22 +0800

    finalize

--- finalize.bat
+++ finalize.bat
@@ -1,3 +1,3 @@
-sqlite3.exe ..\Card\cards.cdb <cmd\finalize.txt
+sqlite3.exe ..\cdb\cards.cdb <cmd\finalize.txt
 sqlite3.exe ..\Card\expansions\beta.cdb <cmd\finalize2.txt
 sqlite3.exe ..\cdb\pre-release.cdb <cmd\finalize2.txt
