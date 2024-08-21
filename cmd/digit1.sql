ac3ae5870d0fbcdeed22f17a1341646d452cc1ca
salix5 <salix5566@gmail.com>
2024-08-22 02:35:14 +0800

    workaround: ignore 強奪

--- cmd/digit1.sql
+++ cmd/digit1.sql
@@ -1,1 +1,2 @@
-select texts.id from datas, texts where datas.id==texts.id and datas.ot!=2 and texts.desc like '%①%';
+select texts.id from datas, texts 
+where datas.id != 45986603 and datas.id==texts.id and datas.ot!=2 and texts.desc like '%①%';
