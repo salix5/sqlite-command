10eb552aa0512edac16c111dc88a1bd9df3c2429
salix5 <salix5566@gmail.com>
2024-08-21 08:40:31 +0800

    update test cmd

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-sqlite3.exe cards.cdb <cmd\test_chs.txt >result\test1.txt
-sqlite3.exe ..\Card\cards.cdb <cmd\test.txt >result\test2.txt
+sqlite3.exe ..\mygo\locales\zh-CN\cards.cdb <cmd\test_chs.txt >result\test1.txt
+sqlite3.exe ..\cdb\cards.cdb <cmd\test.txt >result\test2.txt
 fc result\test1.txt result\test2.txt
 pause
