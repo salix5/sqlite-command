e93fb1957a1e7d0dd0f2d621a8a5b17bc3041955
salix5 <salix5566@gmail.com>
2024-08-22 02:29:56 +0800

    update .gitignore

--- cmd/copy_category.sql
+++ cmd/copy_category.sql
@@ -1,5 +1,4 @@
 ATTACH '../mygo/locales/zh-CN/cards.cdb' AS tmp;
-UPDATE datas SET category=tmp.datas.category
-FROM tmp.datas
+UPDATE datas SET category=tmp.datas.category FROM tmp.datas
 WHERE main.datas.id==tmp.datas.id;
 DETACH tmp;
