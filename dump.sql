e04ef2e6f4b3b15fc716e1ba24ebe03b63bd716d
salix5 <salix5566@gmail.com>
2024-11-27 01:51:33 +0800

    move dump to finalize

--- dump.sql
+++
@@ -1,4 +0,0 @@
-.open ../cdb/cards.cdb
-.output ../cdb/cards.cdb.sql
-.dump datas
-.exit
--- finalize.sql
+++ finalize.sql
@@ -1,7 +1,10 @@
 .open ../cdb/cards.cdb
 .read cmd/copy_category.sql
 .read cmd/finalize_db.sql
+.output ../cdb/cards.cdb.sql
+.dump datas
 
 .open ../cdb/expansions/pre-release.cdb
+.output
 .read cmd/finalize_db2.sql
 .exit
