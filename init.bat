939d6cd4e24881adb365b0865116a24c9d5bcdd2
salix5 <salix5566@gmail.com>
2025-04-17 20:24:48 +0800

    fix init.bat

--- init.bat
+++ init.bat
@@ -1,4 +1,4 @@
-SET url=https://www.sqlite.org/2025/sqlite-tools-win-x64-3490100.zip
+SET url=https://sqlite.org/2025/sqlite-tools-win-x64-3490100.zip
 curl %url% -o sqlite.zip
 tar -xf sqlite.zip sqlite3.exe
 del sqlite.zip
