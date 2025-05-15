8ed9fc22ebf8af36a1349b16391921083ceabd1e
salix5 <salix5566@gmail.com>
2025-05-15 23:39:38 +0800

    use transaction

--- move_data.sql
+++ move_data.sql
@@ -1,7 +1,10 @@
 .open ../cdb/cards.cdb
+PRAGMA foreign_keys=ON;
+BEGIN TRANSACTION;
 ATTACH '../cdb/expansions/pre-release.cdb' AS pre;
 INSERT INTO datas SELECT * FROM pre.datas WHERE pre.datas.id <= 99999999;
 INSERT INTO texts SELECT * FROM pre.texts WHERE pre.texts.id <= 99999999;
 DELETE FROM pre.datas WHERE pre.datas.id <= 99999999;
 DELETE FROM pre.texts WHERE pre.texts.id <= 99999999;
 DETACH pre;
+COMMIT;
