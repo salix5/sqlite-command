191179586540328f0a246fb36bfb663abab9e280
salix5 <salix5566@gmail.com>
2025-05-26 21:49:37 +0800

    fix move_data.sql

--- move_data.sql
+++ move_data.sql
@@ -1,10 +1,11 @@
 .open ../cdb/cards.cdb
 PRAGMA foreign_keys=ON;
-BEGIN TRANSACTION;
 ATTACH '../cdb/expansions/pre-release.cdb' AS pre;
+BEGIN TRANSACTION;
 INSERT INTO datas SELECT * FROM pre.datas WHERE pre.datas.id <= 99999999;
 INSERT INTO texts SELECT * FROM pre.texts WHERE pre.texts.id <= 99999999;
 DELETE FROM pre.datas WHERE pre.datas.id <= 99999999;
 DELETE FROM pre.texts WHERE pre.texts.id <= 99999999;
-DETACH pre;
 COMMIT;
+DETACH pre;
+.exit
