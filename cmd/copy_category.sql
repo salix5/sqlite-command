f3da343075fafda6d248b40114e8374c4190e6c7
salix5 <salix5566@gmail.com>
2025-05-26 21:52:38 +0800

    format

--- cmd/copy_category.sql
+++ cmd/copy_category.sql
@@ -1,5 +1,4 @@
 ATTACH '../mygo/locales/zh-CN/cards.cdb' AS tmp;
-UPDATE datas SET category=tmp.datas.category FROM tmp.datas
-WHERE main.datas.id==tmp.datas.id;
+UPDATE datas SET category=tmp.datas.category FROM tmp.datas WHERE main.datas.id==tmp.datas.id;
 DETACH tmp;
 UPDATE datas SET category=category+4294967296 WHERE category<0;
