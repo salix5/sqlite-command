9aa4f72ac502bb30d55519285d77332b44d66c60
salix5 <salix5566@gmail.com>
2025-07-21 01:34:17 +0800

    update fetch

--- fetch.bat
+++ fetch.bat
@@ -1,7 +1,6 @@
 @echo off
 rmdir /S /Q temp
 mkdir temp
-curl --remote-name https://cdn02.moecube.com:444/ygopro-super-pre/archive/ygopro-super-pre.ypk
 tar -C temp -xf ygopro-super-pre.ypk
 sqlite3 <fetch.sql
 fc result\pre1.txt result\pre2.txt
