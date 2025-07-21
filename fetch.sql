a602dd5b94ae477eef515c8cb49da2ec649cfe9c
salix5 <salix5566@gmail.com>
2025-07-21 22:36:21 +0800

    add integrity checks and cell size check

--- fetch.sql
+++ fetch.sql
@@ -1,8 +1,16 @@
 .open temp/test-release.cdb
+.output
+PRAGMA integrity_check;
+PRAGMA cell_size_check=ON;
+PRAGMA trusted_schema=OFF;
 .output result/pre1.txt
 .read cmd/compare_full.sql
 
 .open ../CardEditor/expansions/beta.cdb
+.output
+PRAGMA integrity_check;
+PRAGMA cell_size_check=ON;
+PRAGMA trusted_schema=OFF;
 .output result/pre2.txt
 .read cmd/compare_full.sql
 .exit
