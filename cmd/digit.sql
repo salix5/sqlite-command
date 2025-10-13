045a2808508512cfe2aa1ae1aeac92c4ce2b44e7
salix5 <salix5566@gmail.com>
2025-10-14 00:00:41 +0800

    add combinations to digit.sql

--- cmd/digit.sql
+++ cmd/digit.sql
@@ -12,3 +12,15 @@ WHERE ot!=2 AND desc LIKE '%④%' ORDER BY id;
 
 SELECT id FROM datas JOIN texts USING (id) 
 WHERE ot!=2 AND desc LIKE '%⑤%' ORDER BY id;
+
+SELECT id FROM datas JOIN texts USING (id) 
+WHERE ot!=2 AND desc LIKE '%①②%' ORDER BY id;
+
+SELECT id FROM datas JOIN texts USING (id) 
+WHERE ot!=2 AND desc LIKE '%①③%' ORDER BY id;
+
+SELECT id FROM datas JOIN texts USING (id) 
+WHERE ot!=2 AND desc LIKE '%②③%' ORDER BY id;
+
+SELECT id FROM datas JOIN texts USING (id) 
+WHERE ot!=2 AND desc LIKE '%①②③%' ORDER BY id;
