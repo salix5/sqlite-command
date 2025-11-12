40227433bb7eec9d7921c6df15dcab11f409f19b
salix5 <salix5566@gmail.com>
2025-11-12 23:13:16 +0800

    refactor copy_category.sql

--- cmd/copy_category.sql
+++ cmd/copy_category.sql
@@ -1,4 +1,8 @@
 ATTACH '../mygo/locales/zh-CN/cards.cdb' AS tmp;
-UPDATE datas SET category=tmp.datas.category FROM tmp.datas WHERE main.datas.id==tmp.datas.id;
+BEGIN TRANSACTION;
+UPDATE datas SET category = tmp.datas.category FROM tmp.datas 
+WHERE main.datas.id == tmp.datas.id AND main.datas.category >= 0 AND category != tmp.datas.category;
+UPDATE datas SET category = tmp.datas.category + 4294967296 FROM tmp.datas 
+WHERE main.datas.id == tmp.datas.id AND main.datas.category < 0 AND category != tmp.datas.category + 4294967296;
+COMMIT;
 DETACH tmp;
-UPDATE datas SET category=category+4294967296 WHERE category<0;
