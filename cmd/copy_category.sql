c1ba0faddcf1a4b9fe35fe8a18ca852bf855a240
salix5 <salix5566@gmail.com>
2025-12-06 17:39:12 +0800

    minor fix

--- cmd/copy_category.sql
+++ cmd/copy_category.sql
@@ -1,8 +1,8 @@
 ATTACH '../mygo/locales/zh-CN/cards.cdb' AS tmp;
 BEGIN TRANSACTION;
 UPDATE datas SET category = tmp.datas.category FROM tmp.datas 
-WHERE main.datas.id == tmp.datas.id AND main.datas.category >= 0 AND category != tmp.datas.category;
+WHERE main.datas.id == tmp.datas.id AND main.datas.category >= 0 AND main.datas.category != tmp.datas.category;
 UPDATE datas SET category = tmp.datas.category + 4294967296 FROM tmp.datas 
-WHERE main.datas.id == tmp.datas.id AND main.datas.category < 0 AND category != tmp.datas.category + 4294967296;
+WHERE main.datas.id == tmp.datas.id AND main.datas.category < 0 AND main.datas.category != tmp.datas.category + 4294967296;
 COMMIT;
 DETACH tmp;
