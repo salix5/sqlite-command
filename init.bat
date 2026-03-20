7c8e62d0cd3890245a57662458a1678a7552a142
salix5 <salix5566@gmail.com>
2026-03-20 20:47:09 +0800

    update sqlite URL to version 3.49.2

--- init.bat
+++ init.bat
@@ -1,4 +1,4 @@
-SET URL=https://www.sqlite.org/2025/sqlite-tools-win-x64-3500300.zip
+SET URL=https://www.sqlite.org/2025/sqlite-tools-win-x64-3490200.zip
 curl %URL% -o sqlite.zip
 tar -xf sqlite.zip sqlite3.exe
 del sqlite.zip
