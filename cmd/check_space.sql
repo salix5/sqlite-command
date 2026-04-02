a132870c1071b45764e0f561d769aa4fbc445c4b
salix5 <salix5566@gmail.com>
2026-04-03 00:35:30 +0800

    use IN clause in check_space.sql

--- cmd/check_space.sql
+++ cmd/check_space.sql
@@ -12,23 +12,20 @@ SELECT id, name FROM texts WHERE desc LIKE '「 ' OR desc LIKE ' 」';
 SELECT id, name FROM texts WHERE desc LIKE '%
 ';
 
-SELECT id, ot FROM datas WHERE ot!=1 AND ot!=2 AND ot!=3;
+SELECT id, ot FROM datas WHERE NOT ot IN (0x1, 0x2, 0x3);
+
+--Check fusion material
 SELECT id, name FROM datas JOIN texts USING (id) WHERE type & 0x40 AND NOT desc like '%+%' AND NOT desc like '%×%' 
 AND setcode!=0xa008 AND NOT desc like '%克力迪斯之牙%' AND not desc like '%海爾摩斯之爪%'
-AND id != 13331639
-AND id != 13857930
-AND id != 31111109
-AND id != 33250142
-AND id != 78734254
-AND id != 6218704
-AND id != 6218705
-AND id != 71398055;
+AND NOT id in (13331639, 13857930, 31111109, 33250142, 78734254, 71398055)
+AND NOT alias in (13331639, 6218704);
 
+--Check synchro material
 SELECT id, name FROM datas JOIN texts USING (id) WHERE type & 0x2000 AND NOT desc like '%+%'
 AND setcode!=0x163
-AND id != 1686814
-AND id != 90884403;
+AND NOT id in (1686814, 90884403);
 
+--Check xyz material
 SELECT id, name FROM datas JOIN texts USING (id) WHERE type & 0x800000 AND NOT desc like '%×%';
 
 SELECT id FROM datas JOIN texts USING (id) WHERE desc like '%。①%';
