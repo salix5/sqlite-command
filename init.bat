ae368f3991b3c4e34ba032ad6a2f26d3e2dd6d3a
salix5 <salix5566@gmail.com>
2026-06-26 20:35:52 +0800

    move to 3.51.3

--- init.bat
+++ init.bat
@@ -1,4 +1,4 @@
-SET URL=https://sqlite.org/2026/sqlite-tools-win-x64-3530200.zip
+SET URL=https://sqlite.org/2026/sqlite-tools-win-x64-3510300.zip
 curl %URL% -o sqlite.zip
 tar -xf sqlite.zip sqlite3.exe
 del sqlite.zip
